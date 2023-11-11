d735ca8291deba75c1a350960e149276e07cc4d0
21dbwls12 <yoojin1117@gmail.com>
2023-11-11 17:36:11 +0900

    add camera, gallery launcher in upload screen

--- app/src/main/java/com/example/myapplication/Screen/InputScreen.kt
+++ app/src/main/java/com/example/myapplication/Screen/InputScreen.kt
@@ -1,5 +1,10 @@
 package com.example.myapplication.Screen
 
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -16,10 +21,15 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,6 +49,32 @@ fun previewSurfaceIn() {
 
 @Composable
 fun InputScreen() {
+    // 갤러리 이미지 uri 객체
+    var selectUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    // 기본 사진 앱 비트맵 객체
+    var takenPhoto by remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+    val context = LocalContext.current
+    // 갤러리 이미지 런쳐
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri ->
+            selectUri = uri
+            takenPhoto = null
+        }
+    )
+    // 카메라 이미지 런쳐
+    val cameraLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicturePreview(),
+        onResult = { photo ->
+            takenPhoto = photo
+            selectUri = null
+        }
+    )
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
@@ -62,8 +98,19 @@ fun InputScreen() {
             horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier.width(300.dp)
         ) {
-            GetiButton(onclick = { /*TODO*/ }, text = "사진찍기")
-            GetiButton(onclick = { /*TODO*/ }, text = "불러오기")
+            GetiButton(
+                onclick = {
+                    // 기본 카메라 앱 실행
+                    cameraLauncher.launch(null)
+                },
+                text = "사진찍기"
+            )
+            GetiButton(
+                onclick = {
+                    launcher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                },
+                text = "불러오기"
+            )
         }
     }
 }
